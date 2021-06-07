d945b21ad6d655a6a470c0eaaa470bbc7ae4f013
sam <sam@arch.com>
2021-06-07 20:43:41 +0530

    dwm logo patch

--- config.def.h
+++ config.def.h
@@ -1,12 +1,32 @@
 /* user and group to drop privileges to */
-static const char *user  = "sam";
-static const char *group = "sam";
+static const char *user  = "nobody";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+  [BACKGROUND] =   "282828",     /* after initialization */
+  [INIT] =   "#076678",
+  [INPUT] =  "#d65d0e",   /* during input */
+  [FAILED] = "#cc241d",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+  /* x	y	w	h */
+  { 0,	3,	1,	3 },
+  { 1,	3,	2,	1 },
+  { 0,	5,	8,	1 },
+  { 3,	0,	1,	5 },
+  { 5,	3,	1,	2 },
+  { 7,	3,	1,	2 },
+  { 8,	3,	4,	1 },
+  { 9,	4,	1,	2 },
+  { 11,	4,	1,	2 },
+
+};
